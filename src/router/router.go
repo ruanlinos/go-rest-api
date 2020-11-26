76202633e3d1913fd38af452df9dea1e74650155
Ruan Linos Alves <ruandeveloper@hotmail.com>
2020-11-25 22:08:49 -0300

    feat(Router): Created the simple router for users.

--- src/router/router.go
+++ src/router/router.go
@@ -1,8 +1,14 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"api/src/router/routes"
+
+	"github.com/gorilla/mux"
+)
 
 //Generate returns the application routes
 func Generate() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+
+	return routes.Configurate(r)
 }
--- src/router/routes/routes.go
+++ src/router/routes/routes.go
@@ -1,6 +1,10 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 //Route represents all api routes.
 type Route struct {
@@ -8,4 +12,15 @@ type Route struct {
 	Method                 string
 	Controller             func(http.ResponseWriter, *http.Request)
 	AuthenticationRequired bool
+}
+
+//Configurate returns all routes configurated.
+func Configurate(r *mux.Router) *mux.Router {
+	routes := userRoutes
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Controller).Methods(route.Method)
+	}
+
+	return r
 }
